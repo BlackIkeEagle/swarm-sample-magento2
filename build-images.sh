fd1c9204dff78e90945724944442f966d7007648
BlackEagle <ike.devolder@gmail.com>
2018-01-21 20:15:17 +0100

    build docker images for the swarm demo
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

---
+++ build-images.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+set -ex
+
+if [[ -z $1 ]]; then
+    echo "give the version to build"; exit 1;
+fi
+version=$1
+
+rm -rf pub/static/* generated/* var/*
+composer install --no-dev
+php bin/magento setup:static-content:deploy -f en_US
+php bin/magento setup:di:compile
+composer dump-autoload -o
+
+docker build --squash=true -f Dockerfile-nginx -t blackikeeagle/swarm-magento2-demo-nginx:$version .
+docker build --squash=true -f Dockerfile-php -t blackikeeagle/swarm-magento2-demo-php:$version .
+
+docker push blackikeeagle/swarm-magento2-demo-nginx:$version
+docker push blackikeeagle/swarm-magento2-demo-php:$version
